091d89f37609a046add9ed5e6daeed0d90c46aa9
David Macharia <macharia3041@gmail.com>
2024-09-24 16:57:52 +0300

    Handled undefined env variables

--- next.config.js
+++ next.config.js
@@ -31,15 +31,7 @@ const nextConfig = withPWA({
 
 const env = process.env.NODE_ENV === "development" ? {} : {
   NEXTAUTH_URL: "http://102.209.68.72:32391",
-  NEXTAUTH_SECRET: "secret!JWTrandom65",
-  // GITHUB_REGISTRY: "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",
-  NEXT_PUBLIC_GITHUB_CLIENT_ID: "Ov23li1JRtxLjAH3IO8D",
-  NEXT_PUBLIC_GITHUB_CLIENT_SECRET: "e3820c1c46a1ade605e5aa1b7881dd0babefe811",
-  NEXT_PUBLIC_VMMS_BACKEND_URL: "http://localhost:3000",
-  NEXT_PUBLIC_GITHUB_OAUTH_ID: "Ov23li1JRtxLjAH3IO8D",
-  AUTH_SECRET: "63APnhoxtwPZCoVzhxq4ROLzw8TEBINMur/2AS1Ymco=",
-  // GITHUB_SECRET: "e3820c1c46a1ade605e5aa1b7881dd0babefe811",
-  // GITHUB_ID: "Ov23li1JRtxLjAH3IO8D"
+  NEXTAUTH_SECRET: "secret!JWTrandom65"
 }
 
 module.exports = {
